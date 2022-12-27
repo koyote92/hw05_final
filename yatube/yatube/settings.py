44c64f84a1c0448342ea874c1c493bd2f6b1a7b5
Roman Inozemtsev <irs1992@yandex.ru>
2022-12-28 00:12:10 +0500

    ok

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#%d7waucy02*7)#x-40(yfn+ij_e8oq7^_*^vc1*@*8(omt#re"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
